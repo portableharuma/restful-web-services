2f80b87a128f3e3e43a792f7aa8084d92cf49140
portableharuma <haruma.cheung@gmail.com>
2026-02-21 20:02:11 -0600

    Created a contorller and exposed API using GetMapping, which is just RequestMapping of type get.

---
+++ src/main/java/com/haruma/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.haruma.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+    // /hello-world
+
+    //"Hello World"
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
