14dd5aff9d96563e9bb96ee2c9e88d54cf02afa0
portableharuma <haruma.cheung@gmail.com>
2026-03-21 11:27:54 -0500

    Implemented the correct HTTP response status for createUser().

--- src/main/java/com/haruma/restful_web_services/user/UserResource.java
+++ src/main/java/com/haruma/restful_web_services/user/UserResource.java
@@ -1,7 +1,12 @@
 package com.haruma.restful_web_services.user;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 //Denotes a REST API
@@ -26,7 +31,9 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri(); // /users/4 => /users/{id}, user.getId
+        return ResponseEntity.created(location).build();
     }
 }
